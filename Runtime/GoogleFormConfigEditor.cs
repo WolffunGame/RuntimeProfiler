6dd39da3f33a0961c6e88cf87cddb2fd5ed2b139
felixngd <danhdat.gamedeveloper@gmail.com>
2023-09-20 14:37:58 +0700

    fix directory error

--- Runtime/GoogleFormConfigEditor.cs
+++ Runtime/GoogleFormConfigEditor.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 
@@ -9,25 +10,37 @@ namespace Wolffun.RuntimeProfiler
         [InitializeOnLoadMethod]
         static void InitGoogleFormConfigEditor()
         {
+            string performanceDirectory = "Assets/Resources/" + SendToGoogleSheet.PERFORMANCE_CONFIG_PATH;
+            string loadingTimeDirectory = "Assets/Resources/" + SendToGoogleSheet.LOADING_TIME_CONFIG_PATH;
+
+            if (!System.IO.Directory.Exists(performanceDirectory))
+            {
+                System.IO.Directory.CreateDirectory(performanceDirectory);
+            }
+
             PerformanceGoogleFormConfig config = Resources.Load<PerformanceGoogleFormConfig>(SendToGoogleSheet.PERFORMANCE_CONFIG_PATH);
             if (config == null)
             {
                 config = ScriptableObject.CreateInstance<PerformanceGoogleFormConfig>();
-                AssetDatabase.CreateAsset(config, "Assets/Resources/" + SendToGoogleSheet.PERFORMANCE_CONFIG_PATH + ".asset");
+                AssetDatabase.CreateAsset(config, performanceDirectory + ".asset");
                 AssetDatabase.SaveAssets();
-                Debug.Log("PerformanceGoogleFormConfig created at Assets/Resources/" + SendToGoogleSheet.PERFORMANCE_CONFIG_PATH);
+                Debug.Log("PerformanceGoogleFormConfig created at " + performanceDirectory);
+            }
+
+            if (!System.IO.Directory.Exists(loadingTimeDirectory))
+            {
+                System.IO.Directory.CreateDirectory(loadingTimeDirectory);
             }
 
             LoadingTimeGoogleFormConfig config1 = Resources.Load<LoadingTimeGoogleFormConfig>(SendToGoogleSheet.LOADING_TIME_CONFIG_PATH);
             if (config1 == null)
             {
                 config1 = ScriptableObject.CreateInstance<LoadingTimeGoogleFormConfig>();
-                AssetDatabase.CreateAsset(config1, "Assets/Resources/" + SendToGoogleSheet.LOADING_TIME_CONFIG_PATH + ".asset");
+                AssetDatabase.CreateAsset(config1, loadingTimeDirectory + ".asset");
                 AssetDatabase.SaveAssets();
-                Debug.Log("LoadingTimeGoogleFormConfig created at Assets/Resources/" + SendToGoogleSheet.LOADING_TIME_CONFIG_PATH);
+                Debug.Log("LoadingTimeGoogleFormConfig created at " + loadingTimeDirectory);
             }
         }
     }
 }
-
 #endif
